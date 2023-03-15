e3b553f50d07aa6b87a90946b3f2b0a3e333570c
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-03-15 18:22:01 +0900

    [Silver III] Title: -2진수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2089. －2진수/－2진수.py
+++ 백준/Silver/2089. －2진수/－2진수.py
@@ -1,16 +1,21 @@
 import sys
 
-N = int(sys.stdin.readline())
-if not N:
-    sys.stdout.write('0')
-    exit()
-res = ''
-while N:
-    if N%(-2):
-        res = '1' + res
-        N = N//-2 + 1
-    else:
-        res = '0' + res
-        N //= -2
-        
-sys.stdout.write(res)
+input = sys.stdin.readline
+
+n = int(input())
+result = []
+
+if n == 0:
+    print(0)
+else:
+    while n != 0:
+        if n % (-2) != 0:
+            result.append(1)
+            n = (n // -2) + 1
+        else:
+            result.append(0)
+            n = (n // -2)
+
+    for n in result[::-1]:
+        print(n, end='')
+    print()
