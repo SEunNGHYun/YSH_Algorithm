623d10992680c4d565052fa505a9214281fac5e6
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-05-31 15:41:29 +0900

    [Silver IV] Title: 좋은 단어, Time: 136 ms, Memory: 32956 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.py
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -1,19 +1,19 @@
 import sys
+
 input = sys.stdin.readline
- 
+
 n = int(input())
-cnt = 0
- 
+count = 0
 for _ in range(n):
-    s = input().rstrip()
-    stack = []
- 
-    for i in range(len(s)):
-        if stack and s[i] == stack[-1]:
-            stack.pop()
-        else:
-            stack.append(s[i])
- 
-    if not stack:
-        cnt += 1
-print(cnt)
+    s = list(input().strip())
+    stack = [s[0]]
+    leng_r = len(s)
+    if (leng_r % 2) == 0:
+        for i in range(1, leng_r):
+            if stack and s[i] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+    if len(stack) == 0:
+        count += 1
+print(count)
