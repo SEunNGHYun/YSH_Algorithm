8287bea7c92b4f6a020eb9754b8767139bf706b1
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-06-13 17:04:09 +0900

    [Silver III] Title: 프린터 큐, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+  n, m = map(int, input().split())
+  works = list(map(int, input().split()))
+  works_li = [{ "id" : i, "val" : works[i]} for i in range(n)]
+  turn = 0
+  
+  while True:
+    if n == 0:
+      break
+    check = True
+    for j in range(1, n):
+      if works_li[0]['val'] < works_li[j]['val']:
+        check = False
+        works_li.append(works_li.pop(0))
+        break
+
+    if check : 
+        n -= 1
+        if works_li[0]['id'] == m : 
+          turn += 1
+          break
+        else : 
+          turn += 1
+          works_li.pop(0)
+    
+  print(turn)
