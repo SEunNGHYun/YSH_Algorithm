63721316b823f0a9041811b58fd1b94c694e055d
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-06-22 17:01:34 +0900

    [Silver III] Title: 영식이의 손가락, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+check = [1, 2, 3, 4, 5, 4, 3, 2]
+f = int(input())
+use = int(input())
+result = 0
+
+if (use < 3):
+  for i in range(8):
+    if (check[i] == f) and (use == 0):
+      break
+    elif (check[i] == f):
+      use -= 1
+    result += 1
+else:
+  if f == 1 or f == 5 :
+    result = (use) * 8
+    use = 0
+    for i in range(8):
+      if (check[i] == f) and (use == 0):
+        break
+      result += 1
+  else :
+    result = (use // 2) * 8
+    use = (use % 2)
+    for i in range(8):
+      if (check[i] == f) and (use == 0):
+        break
+      elif (check[i] == f):
+        use -= 1
+      result += 1
+
+print(result)
