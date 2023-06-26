6e22e6e2b96f6c40fee55edfd9a5608278fcce25
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-06-26 22:10:32 +0900

    [Bronze III] Title: 플러그, Time: 232 ms, Memory: 39232 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.py
+++ 백준/Bronze/2010. 플러그/플러그.py
@@ -3,11 +3,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-result = [0 for _ in range(n+1)]
+result = [-1 for _ in range(n-1)] + [0]
 
-for i in range(1, n+1):
-  result[i] = int(input())
-  if i > 1 :
-    result[i-1] -= 1
-
+for i in range(n):
+  result[i] += int(input())
+  
 print(sum(result))
