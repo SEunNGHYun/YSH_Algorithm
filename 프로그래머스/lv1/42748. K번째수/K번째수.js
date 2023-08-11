bcf76da28611e835d8c444437353374906cad111
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-08-11 15:31:24 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,6 +1,5 @@
 function solution(array, commands) {
-    var answer = commands.map((arr) => {
-        return array.slice(arr[0] -1, arr[1]).sort((a,b)=>a-b)[arr[2]-1]
-    })
+    var answer = commands.map((arr) => array.slice(arr[0] -1, arr[1]).sort((a,b)=>a-b)[arr[2]-1]
+    )
     return answer;
 }
