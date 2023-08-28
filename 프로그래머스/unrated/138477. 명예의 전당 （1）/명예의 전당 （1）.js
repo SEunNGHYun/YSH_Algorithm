a5d0e97deb7320228f123004fcbba98163f381a0
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-08-28 17:36:39 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -4,12 +4,12 @@ function solution(k, score) {
     for(let i = 0; i < score.length; i++){
             if (i < k) {
                 li.push(score[i])
+                li.sort((a,b) => b-a)
             }
-            if(score[i] > Math.min(...li)){
+            else if(score[i] > Math.min(...li)){
                     li.pop()
                     li.push(score[i])
                     li.sort((a,b) => b-a)
-                
             }
             answer.push(li[li.length-1])
     }    
