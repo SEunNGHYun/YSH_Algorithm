fc568d9cf4ae243587043d39eda6f4f6289906b8
Seung-Hyun <48947537+SEunNGHYun@users.noreply.github.com>
2023-11-15 14:08:47 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,7 @@
+def solution(age):
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    s_age = str(age)
+    answer = ''
+    for s in s_age: 
+        answer += alpha[int(s)]
+    return answer
